824246d0bbe055ca8a7e75435a55563932d87647
jrote1 <jrote1@me.com>
2014-05-06 14:19:14 +0100

    Fixed Bug

--- packages/simple_exportable/simple_exportable.dart
+++ packages/simple_exportable/simple_exportable.dart
@@ -2,7 +2,7 @@ import 'dart:mirrors';
 
 class Exportable {
   static ClassMirror _getActualTypeFromVariableMirror(VariableMirror type) {
-    if (reflectClass(List) == type.type) {
+    if (reflectClass(List).qualifiedName == type.type.qualifiedName) {
       return type.type.typeArguments[0];
     }
     return type.type;
