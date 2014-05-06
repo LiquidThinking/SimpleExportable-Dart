570a479cbe15f5a3c45d608fa6cb94743c07f622
jrote1 <jrote1@me.com>
2014-05-06 11:35:24 +0100

    Fixed DateTime bug

--- packages/simple_exportable/simple_exportable.dart
+++ packages/simple_exportable/simple_exportable.dart
@@ -17,13 +17,14 @@ class Exportable {
 		for (DeclarationMirror declarationMirror in thisMirror.type.declarations.values) {
 			if (declarationMirror is VariableMirror) {
 				var name = MirrorSystem.getName(declarationMirror.simpleName);
+				;
 				var matchedKeys = json.keys.where((String key)=>key.toLowerCase() == name.toLowerCase());
 				if (matchedKeys.length > 0) {
 					var value = json[matchedKeys.elementAt(0)];
 					if (isExportableClass(declarationMirror)) {
 						thisMirror.setField(declarationMirror.simpleName, getInstatiatedTypeForJson(declarationMirror.type.reflectedType, value));
 					} else {
-						if (value is DateTime) thisMirror.setField(declarationMirror.simpleName, (value as DateTime).toUtc().toString()); else thisMirror.setField(declarationMirror.simpleName, value);
+						if (declarationMirror.type.reflectedType == DateTime) thisMirror.setField(declarationMirror.simpleName, DateTime.parse(value as String).toLocal()); else thisMirror.setField(declarationMirror.simpleName, value);
 					}
 				}
 			}
