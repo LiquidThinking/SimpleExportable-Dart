c485870c8817019d1dec9dd18db941efae9eb70a
jrote1 <jrote1@me.com>
2014-05-06 16:54:25 +0100

    rebuilt core

--- packages/simple_exportable/simple_exportable.dart
+++ packages/simple_exportable/simple_exportable.dart
@@ -1,30 +1,15 @@
 import 'dart:mirrors';
 
 class Exportable {
-  static ClassMirror _getActualTypeFromVariableMirror(VariableMirror type) {
-    if (reflectClass(List).qualifiedName == type.type.qualifiedName) {
-      return type.type.typeArguments[0];
-    }
-    return type.type;
+  static bool _isExportableClass(Type type) {
+    return reflectClass(type).superclass.reflectedType == Exportable;
   }
 
-  static Type _getActualType(Type type) {
-    ClassMirror reflectedType = reflectClass(type);
-    if (reflectedType is List) {
-      return reflectedType.typeArguments[0].reflectedType;
+  static Type _getActualVariableType(VariableMirror variableMirror) {
+    if (variableMirror.type.qualifiedName == reflectType(List).qualifiedName) {
+      return variableMirror.type.typeArguments[0].reflectedType;
     }
-    return reflectedType.reflectedType;
-  }
-
-  static bool _isExportableClass(VariableMirror variableMirror) {
-    
-    var actualType = _getActualTypeFromVariableMirror(variableMirror);
-    if (actualType is ClassMirror) {
-      if (actualType.superclass.reflectedType == Exportable) {
-        return true;
-      }
-    }
-    return false;
+    return variableMirror.type.reflectedType;
   }
 
   static bool _isJsonSupported(value) {
@@ -44,23 +29,26 @@ class Exportable {
   }
 
   static Object getInstatiatedTypeForJson(Type type, dynamic json) {
-
-    if (reflectClass(type) == reflectClass(List) || json is List) {
-      return json.map((e) => getInstatiatedTypeForJson(_getActualType(type), e));
+    if (json is List) {
+      var result = [];
+      json.forEach((e) => result.add(getInstatiatedTypeForJson(type, e)));
+      return result;
     } else {
+
       var result = reflectClass(type).newInstance(const Symbol(''), []).reflectee;
       InstanceMirror thisMirror = reflect(result);
       for (DeclarationMirror declarationMirror in thisMirror.type.declarations.values) {
         if (declarationMirror is VariableMirror) {
           var name = MirrorSystem.getName(declarationMirror.simpleName);
-          ;
+
+          var actualVariableType = _getActualVariableType(declarationMirror);
+
           var matchedKeys = json.keys.where((String key) => key.toLowerCase() == name.toLowerCase());
           if (matchedKeys.length > 0) {
             var value = json[matchedKeys.elementAt(0)];
-
-            if (_isExportableClass(declarationMirror)) {
-              thisMirror.setField(declarationMirror.simpleName, getInstatiatedTypeForJson(declarationMirror.type.reflectedType, value));
-            } else if (_isJsonSupported(value)) {
+            if (_isExportableClass(actualVariableType)) {
+              thisMirror.setField(declarationMirror.simpleName, getInstatiatedTypeForJson(actualVariableType, value));
+            } else {
               thisMirror.setField(declarationMirror.simpleName, _importSimpleValue(declarationMirror.type.reflectedType, value));
             }
           }
@@ -77,12 +65,10 @@ class Exportable {
       if (declarationMirror is VariableMirror) {
         var name = MirrorSystem.getName(declarationMirror.simpleName);
         var value = thisMirror.getField(declarationMirror.simpleName).reflectee;
-        result[name] = _isExportableClass(declarationMirror) ? getMapFromObject(value) : value;
+        result[name] = _isExportableClass(declarationMirror.type.reflectedType) ? getMapFromObject(value) : value;
       }
     }
 
     return result;
   }
-
-
 }
---
+++ test/unit_tests.dart
@@ -0,0 +1,65 @@
+import 'dart:convert';
+import 'package:unittest/unittest.dart';
+import 'package:unittest/html_config.dart';
+import 'package:simple_exportable/simple_exportable.dart';
+
+class FlatClass extends Exportable {
+  int id;
+  String name;
+}
+
+class LayerdClass extends Exportable{
+  FlatClass flatClass;
+  String name;
+}
+
+class LayerdClasses extends Exportable{
+  List<FlatClass> flatClasses;
+  String name;
+}
+
+void main() {
+  useHtmlConfiguration();
+
+  test('for flat object fills properties', () {
+    var json = JSON.decode('''{"id":1,"name":"John"}''');
+    FlatClass result = Exportable.getInstatiatedTypeForJson(FlatClass, json);
+
+    expect(result.id, 1);
+    expect(result.name, "John");
+  });
+
+  test('for list of flat objects fills properties', () {
+    var json = JSON.decode('''[{"id":1,"name":"John"},{"id":2,"name":"Fred"}]''');
+    List<FlatClass> result = Exportable.getInstatiatedTypeForJson(FlatClass, json);
+
+    expect(result[0].id, 1);
+    expect(result[0].name, "John");
+    
+    expect(result[1].id, 2);
+    expect(result[1].name, "Fred");
+  });
+  
+  test('for object with sub class fills properties', () {
+      var json = JSON.decode('''{"flatClass": {"id":1,"name":"John"}, "name": "John"}''');
+      LayerdClass result = Exportable.getInstatiatedTypeForJson(LayerdClass, json);
+
+      expect(result.name, "John");
+      
+      expect(result.flatClass.id, 1);
+      expect(result.flatClass.name, "John");
+    });
+  
+  test('for object with list of sub class fills properties', () {
+        var json = JSON.decode('''{"flatClasses": [{"id":1,"name":"John"},{"id":2,"name":"Fred"}], "name": "John"}''');
+        LayerdClasses result = Exportable.getInstatiatedTypeForJson(LayerdClasses, json);
+
+        expect(result.name, "John");
+        
+        expect(result.flatClasses[0].id, 1);
+        expect(result.flatClasses[0].name, "John");
+        
+        expect(result.flatClasses[1].id, 2);
+                expect(result.flatClasses[1].name, "Fred");
+      });
+}
